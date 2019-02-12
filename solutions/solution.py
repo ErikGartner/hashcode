fc23c1d270ad7841c9d4c472b7247196e4e0d5bb
Erik Gärtner <erik@gartner.io>
2019-02-12 22:00:20 +0100

    Always print what problem is being worked on

--- solutions/solution.py
+++ solutions/solution.py
@@ -11,7 +11,7 @@ def solve(in_file, seed=0, debug=True):
     """
     Generates a solution, use seed to ensure that we can search over many different.
     """
-    dprint('Running: {}'.format(in_file))
+    print('Running: {}'.format(in_file))
     random.seed(seed)
 
     # Use progressbar to display progress in the solution
