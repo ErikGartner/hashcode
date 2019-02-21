1de53dfe0465c02df594e45c03b8efd0c0abc9ea
Erik Gärtner <erik@gartner.io>
2019-02-21 20:22:54 +0100

    Add highscoring function

--- README.md
+++ README.md
@@ -7,4 +7,9 @@
 3. Write `solutions/utils/parser.py`
     1. `parse_in()`. Should create fast data structures for the problem.
     2. `write_ans()`. Should create valid answer file.
-    3. (Optional) `parse_ans()` to parse answers
+4. Write a basic strategy in `solutions/strategies/default.py`
+5. Submit first score to Google for verification
+6. Implement
+  - `parse_ans()` in `solutions/utils/parser.py` to parse answers
+  - Scoring in `solutions/score.py` by implementing `do_scoring()`
+  - Advanced strategies in `solutions/strategies/`
--- solutions/score.py
+++ solutions/score.py
@@ -1,11 +1,17 @@
 import glob
 import os
+import json
 
 from .utils.parser import parse_ans
 
 
-def score_answers():
+def do_scoring(ans):
+    """Implement me to do actual scoring from answer"""
+    return 0
 
+
+def score_answers():
+    """Scores all answers"""
     for ans_file in sorted(glob.glob(os.path.join('out', '*.ans'))):
         out_file = ans_file.replace('.ans', '.score')
 
@@ -13,14 +19,29 @@ def score_answers():
             # Already scored, skip file
             continue
 
-        print('Scoring {} -> {}'.format(ans_file, out_file))
         ans = parse_ans(ans_file)
-
         score = do_scoring(ans)
+        print('Scoring {} -> {}: {}'.format(ans_file, out_file, score))
 
         with open(out_file, 'w') as f:
             f.write('{}'.format(score))
 
+    update_highscore()
 
-def do_scoring(ans):
-    return 0
+
+def update_highscore():
+    """Goes over all scores and finds the best answers"""
+    scores = {}
+    for score_file in sorted(glob.glob(os.path.join('out', '*.score'))):
+        problem = score_file.rsplit('_', 2)[0].split('/')[1]
+        ans_file = score_file.replace('.score', '.ans')
+
+        with open(score_file, 'r') as f:
+            score = int(f.read())
+
+        current = scores.get(problem, {'ans': '', 'score': -1})
+        if current['score'] < score:
+            scores[problem] = {'ans': ans_file, 'score': score}
+
+    with open('out/highscores.json', 'w') as f:
+        json.dump(scores, f, indent=2)
