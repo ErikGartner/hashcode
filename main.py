a7a294fdd402eef1d286831a8868b49598590e4f
Erik Gärtner <erik@gartner.io>
2019-02-23 23:24:04 +0100

    Add profiling

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
 import argparse
 import glob
 import os
+import profile
+import pstats
 
 from solutions import solve, score_answers
 from solutions.utils import status
@@ -10,7 +12,10 @@ def main(problem_filter, strategy, seed, debug):
     status.DEBUG = debug
 
     for in_file in sorted(glob.glob(os.path.join('data', problem_filter) + '*')):
-        solve(in_file, strategy, seed, debug)
+        try:
+            solve(in_file, strategy, seed, debug)
+        except KeyboardInterrupt as e:
+            pass
 
 
 def parse_args():
@@ -24,6 +29,8 @@ def parse_args():
                         help='seed for random functions')
     parser.add_argument('--debug', action='store_true',
                         help='show messages / progressbars')
+    parser.add_argument('--profile', action='store_true',
+                        help='profile the code')
     parser.add_argument('--score', action='store_true',
                         help='score answers')
     args = parser.parse_args()
@@ -34,5 +41,16 @@ if __name__ == '__main__':
     args = parse_args()
     if args.score:
         score_answers()
+
+    if args.profile:
+        print('Profiling...')
+        profile.runctx('m(p, st, s, d)',
+                       {'m': main,
+                        'p': args.problem,
+                        'st': args.strategy,
+                        's': args.seed,
+                        'd': args.debug}, {}, filename='profile_log')
+        p = pstats.Stats('profile_log')
+        p.strip_dirs().sort_stats('cumulative').print_stats()
     else:
         main(args.problem, args.strategy, args.seed, args.debug)
