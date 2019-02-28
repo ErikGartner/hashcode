c64b150d1582e6625cef8933300de35f0aecad1f
Erik Gärtner <erik@gartner.io>
2019-02-28 19:54:12 +0100

    Add

--- solutions/strategies/default.py
+++ solutions/strategies/default.py
@@ -6,6 +6,8 @@ from ..utils import parse_in, write_ans, dprint, progressbar
 from .utils import *
 from solutions.score import score_pair
 
+from ..utils.parser import Slide
+
 
 def solve(photos, seed, debug):
 
@@ -16,6 +18,21 @@ def solve(photos, seed, debug):
 
     used_photos = set()
     tag_counter = tag_info(photos)
+
+    slides = [
+        # Set first slide as a horizontal with many tags
+        Slide([hor_nbr_tags.pop(0)])
+    ]
+
+    # Add to used photos
+    used_photos.add(Slide.ids[0])
+
+    # Until all photos are used
+    while len(used_photos) < len(photos):
+
+
+
+
 
 
 
