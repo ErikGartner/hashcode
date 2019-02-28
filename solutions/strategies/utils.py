e6fa1c2e00c6fef116a4fa33cfa877521d71809a
Johan Ahlqvist <johan_ahlkvist@hotmail.com>
2019-02-28 19:38:17 +0100

    add tag map

--- solutions/strategies/utils.py
+++ solutions/strategies/utils.py
@@ -5,3 +5,13 @@ def tag_info(photos):
     tag_counter = Counter()
     tag_list = [tag_counter.update(photo.tags) for photo in photos]
     return tag_counter
+
+def tag_groups(photos):
+    groups = {}
+    for photo in photos:
+        for tag in photo.tags:
+            if tag not in groups:
+                groups[tag] = set([photo.id])
+            else:
+                groups[tag].add(photo.id)
+    return tag_groups
